1a23bcb425d3f353b179de40a374bb805fc72014
Manan <manan511shah@gmail.com>
2023-10-11 18:46:25 +0530

    Minor bug fixed

--- forest/constant/__init__.py
+++ forest/constant/__init__.py
@@ -16,7 +16,7 @@ COLUMN = ['elevation','aspect','slope','horizontal_distance_to_hydrology','Verti
           ,'soil_type_9','soil_type_10','soil_type_11','soil_type_12','soil_type_13','soil_type_14','soil_type_15','soil_type_16'
           ,'soil_type_17','soil_type_18','soil_type_19','soil_type_20','soil_type_21','soil_type_22','soil_type_23','soil_type_24'
           ,'soil_type_25','soil_type_26','soil_type_27','soil_type_28','soil_type_29','soil_type_30','soil_type_31','soil_type_32'
-          ,'soil_type_33','soil_type_34','soil_type_35','soil_type_36','soil_type_37','soil_type_38','soil_type_39','soil_type_40'
+          ,'soil_type_33','soil_type_34','soil_type_35','soil_type_36','soil_type_37','soil_type_38','soil_type_39','soil_type_40','class'
 ]
 
 #training pipeline related varibales
