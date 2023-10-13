8bc987325b00e749db22ce848921a3774b4dfbc4
Manan <manan511shah@gmail.com>
2023-10-13 17:00:21 +0530

    Frontend train route completed and tested

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import cross_origin,CORS
 import os,sys,json
 from forest.loggers import logging
 from forest.exception import ForestException
-
+from forest.pipeline.pipeline import Pipeline
 
 app = Flask(__name__)
 
@@ -12,6 +12,12 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/train',methods=['POST'])
+@cross_origin()
+def train():
+    pipeline = Pipeline()
+    pipeline.run_pipeline()
+    return render_template('index.html',prediction_text = "Model training completed")
 
 if __name__ == "__main__":
     app.run()
